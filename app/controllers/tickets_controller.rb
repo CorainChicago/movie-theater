8142e860e32d18d177aa95b07dcd7c03f723cf96
CorainChicago <chaysmagan@gmail.com>
2016-02-14 22:13:57 -0600

    start to build new ticket form

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,23 @@
 class TicketsController < ApplicationController
+  
+  def new
+    @ticket = Ticket.new(ticket_params)
+    if @ticket.valid?
+      @ticket.save
+    else
+      @errors = @ticket.errors.full_messages
+      render 'tickets/new'
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def ticket_params
+    params.require(:ticket).permit(:email, :credit_card) 
+    #params.require(:ticket).permit(:first_name, :last_name, :showing_id, :email, :credit_card) 
+
+  end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,4 @@
 class Movie < ActiveRecord::Base
   has_many :showings, class_name: "Show"
-  
   has_many :ticket_sales, through: :showings,  :source => :tickets
 end
