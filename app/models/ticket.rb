bee03cfbd071e25f1e6a5431b8e079f5a2bc7f54
CorainChicago <chaysmagan@gmail.com>
2016-02-16 15:55:50 -0600

    add validation to Ticket model

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,7 @@
 class Ticket < ActiveRecord::Base
   belongs_to :show
 
+  validates :show_id, :first_name, :last_name, :email_address, :credit_card_number, :credit_card_expiration_date, presence: true
+
   
 end
