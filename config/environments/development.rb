b610cc6c66ad80eda0fd5d51cdc102a12d3d8399
CorainChicago <chaysmagan@gmail.com>
2016-02-16 11:14:32 -0600

    fix format in environment/developemt for syntax error

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,7 +51,9 @@ Rails.application.configure do
    :password             => ENV['GMAIL_PASSWORD'],
 
   }
-  config.action_mailer.default_url_options = { "host: localhost:3000" }
+  
+  config.action_mailer.default_url_options = { host: "localhost:3000" }
+
 
 
 end
