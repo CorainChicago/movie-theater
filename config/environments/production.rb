b95a6d6a5db9873e07429de89977a071ec68cfee
CorainChicago <chaysmagan@gmail.com>
2016-02-15 22:52:15 -0600

    add missing commas in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
-   :domain               =>'movietheater.herokuapp.com'
+   :domain               =>'movietheater.herokuapp.com',
    :user_name            => ENV['GMAIL_USERNAME'],
    :password             => ENV['GMAIL_PASSWORD'],
    :authentication       => "plain",
