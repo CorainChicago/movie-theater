fe28a71483a9f5b2548a0352e4c6218f010cb133
CorainChicago <chaysmagan@gmail.com>
2016-02-18 09:54:12 -0600

    add filter and column customization to Show

--- app/admin/show.rb
+++ app/admin/show.rb
@@ -1,6 +1,20 @@
 ActiveAdmin.register Show do
   permit_params :movie_id, :screen_id, :starting_time
 
+  filter :movie
+  filter :screen
+
+  index do 
+    column :id
+    column ("Movie") { |show| show.movie.title }
+    column ("Movie Rating") { |show| show.movie.rating }
+    column ("Movie Length") { |show| show.movie.length }
+    column :seats_sold
+    column ("Seating Capacity") { |show| show.screen.seat_count }
+    column :starting_time
+  end
+
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
