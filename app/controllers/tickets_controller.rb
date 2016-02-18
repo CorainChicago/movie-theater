a219ceb5e8795536b655fb67f013b59f4bc7608d
CorainChicago <chaysmagan@gmail.com>
2016-02-18 14:09:10 -0600

    update form for the date_selector to user Time render new ticke with errors and taking out the local attribute for age (caused errors)

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -9,21 +9,24 @@ class TicketsController < ApplicationController
     @ticket = Ticket.new(ticket_params)
     @show = Show.find(params[:show_id])
     @ticket.show_id = @show.id
-    cc_valid = @ticket.check_card_is_valid?(@ticket.credit_card_number.to_i)
-    if @ticket.valid? && cc_valid
+    @age = [ticket_params["age(1i)"], ticket_params["age(2i)"], ticket_params["age(3i)"]]
+    @age = Date.new(@age[0].to_i, @age[1].to_i, @age[2].to_i)
+    # cc_valid = @ticket.check_card_is_valid?(@ticket.credit_card_number.to_i).to_date
+    if @ticket.valid? 
+
       @ticket.credit_card_number = @ticket.credit_card_number.to_s.chars[-4..-1]
       @ticket.save
       @ticket.show.increment!(:seats_sold)
       TicketMailer.ticket_receipt(@ticket).deliver_now
-      redirect_to show_ticket_path(@show.id, @ticket.id)
+      redirect_to show_ticket_path([@ticket.id, @show.id])
     else
       @errors = @ticket.errors.full_messages
-      render 'tickets/new'
+      render 'tickets/new', locals: {ticket: @ticket.age = nil}
     end
   end
 
   def show
-    @ticket = Ticket.find(ticket_params) 
+    @ticket = Ticket.find(params[:id]) 
     @show = Show.find(params[:show_id])
   end
 
