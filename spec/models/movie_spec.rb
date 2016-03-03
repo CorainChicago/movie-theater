a5ac9e0b3887a6879230881395c7c2affe90c1ba
CorainChicago <chaysmagan@gmail.com>
2016-03-03 12:26:48 -0600

    setup rspec testing to run

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:movie) { Movie.new }
+
+  describe "validations" do
+    it "is valid when " do
+        movie.valid?
+        expect(movie.errors).to_not be_empty
+      end
+  end
 end
